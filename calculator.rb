6bfc70f55ac4c840b8cb7e29c7d3b8c1e3f43beb
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-12 19:55:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,6 @@
-
 first_number = 6
 second_number = 14
-def calc_test
-  sum = 6 + 14
-  difference = 6 - 14
-  product = 6 * 14
-  quotient = 6 / 14
-end
-calc_test
+sum = 6 + 14
+difference = 6 - 14
+product = 6 * 14
+quotient = 6 / 14
